fe40701507fc79793d724d04a2eb58c0ca2fa643
mroaizielmi <mroaizielmi@gmail.com>
2017-08-10 03:11:33 +0800

    Add files via upload

--- js/index_user_scripts.js
+++ js/index_user_scripts.js
@@ -7,7 +7,47 @@
  */
  function register_event_handlers()
  {
-    
+//    START
+
+	$( "#submit_form_button" ).click(function() {
+	  submit_to_joomla();
+	});
+
+	function submit_to_joomla(){ 
+		var jform_username = document.getElementById("username").value;
+		var jform_password = document.getElementById("password").value;
+
+		var option = "com_mobile_extension";
+		var task = "user.login";
+		$.ajax({
+                type: "POST",
+                url: "http://mob.cordovaapp.com/front_end/api_login.php",
+                data: {
+                    username: jform_username,
+                    password: jform_password
+                },
+                success: function (ret) {
+                    var loginDetails = {};
+                    console.log(ret);
+                    if (ret.indexOf("success") > -1) {
+                        alert("Logged in successfully!");
+                        loginDetails.email = jform_username;
+                        loginDetails.password = "NaN";
+                        localStorage.setItem("login", btoa(JSON.stringify(loginDetails)));
+                       // SET LANDING PAGE AFTER LOGIN
+                         activate_page("#dashboard");
+                    } else {
+                        alert("Error in details or you have not activated your account. Check spam box too for activation mail!");
+                    }
+                },
+                error: function(err) {
+                    console.log(err);
+                    alert("Conection error!");
+                }
+            });
+	}
+     
+//     END
     
      /* button  Back */
     $(document).on("click", ".uib_w_9", function(evt)
@@ -25,44 +65,40 @@
          return false;
     });
     
-        /* button  Button */
-    
-    
-        /* button  Button */
-    
-    
-        /* button  Button */
-    $(document).on("click", ".uib_w_7", function(evt)
+        /* button  Logout */
+    $(document).on("click", ".uib_w_11", function(evt)
     {
-         /*global activate_page */
-         activate_page("#menu"); 
+       localStorage.removeItem("login");
+        activate_page("#mainpage");
          return false;
     });
     
-        /* button  Button */
-    $(document).on("click", ".uib_w_2", function(evt)
+        /* button  Cordova */
+    $(document).on("click", ".uib_w_6", function(evt)
     {
          /*global activate_page */
-         activate_page("#Cordova"); 
+         activate_page("#cordova"); 
          return false;
     });
     
-        /* button  .uib_w_18 */
-    
-    
-        /* button  .uib_w_18 */
-    $(document).on("click", ".uib_w_18", function(evt)
+        /* button  On Sistem */
+    $(document).on("click", ".uib_w_19", function(evt)
     {
-         /* Other options: .modal("show")  .modal("hide")  .modal("toggle")
-         See full API here: http://getbootstrap.com/javascript/#modals 
-            */
-        
-         $(".uib_w_19").modal("toggle");  
+         /*global activate_page */
+         activate_page("#sistemcordova"); 
+         return false;
+    });
+    
+        /* button  Off Sistem */
+    $(document).on("click", ".uib_w_24", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#cordova"); 
          return false;
     });
     
         /* button  Pendapatan */
-    $(document).on("click", ".uib_w_3", function(evt)
+    $(document).on("click", ".uib_w_7", function(evt)
     {
          /*global activate_page */
          activate_page("#pendapatan"); 
@@ -70,203 +106,134 @@
     });
     
         /* button  Bayaran */
-    $(document).on("click", ".uib_w_20", function(evt)
+    $(document).on("click", ".uib_w_28", function(evt)
     {
          /*global activate_page */
-         activate_page("#Bayaran"); 
+         activate_page("#bayaran"); 
          return false;
     });
     
         /* button  Ranking */
-    $(document).on("click", ".uib_w_21", function(evt)
+    $(document).on("click", ".uib_w_29", function(evt)
     {
          /*global activate_page */
-         activate_page("#Ranking"); 
+         activate_page("#ranking"); 
          return false;
     });
     
         /* button  Info Bank */
-    $(document).on("click", ".uib_w_22", function(evt)
+    $(document).on("click", ".uib_w_30", function(evt)
     {
          /*global activate_page */
-         activate_page("#InfoBank"); 
+         activate_page("#infobank"); 
          return false;
     });
     
         /* button  Idea Anda */
-    $(document).on("click", ".uib_w_4", function(evt)
+    $(document).on("click", ".uib_w_8", function(evt)
     {
          /*global activate_page */
-         activate_page("#IdeaAnda"); 
+         activate_page("#ideaanda"); 
          return false;
     });
     
-        /* button  Info */
-    $(document).on("click", ".uib_w_10", function(evt)
+        /* button  Off Sistem */
+    
+    
+        /* button  Off Sistem */
+    $(document).on("click", ".uib_w_47", function(evt)
     {
          /*global activate_page */
-         activate_page("#Info"); 
+         activate_page("#cordova"); 
          return false;
     });
     
-        /* button  .uib_w_33 */
-    $(document).on("click", ".uib_w_33", function(evt)
-    {
-         /*global activate_page */
-         activate_page("#menu"); 
-         return false;
-    });
-    
-        /* button  .uib_w_34 */
-    $(document).on("click", ".uib_w_34", function(evt)
-    {
-         /*global activate_page */
-         activate_page("#menu"); 
-         return false;
-    });
-    
-        /* button  .uib_w_35 */
-    
-    
-        /* button  .uib_w_36 */
-    
-    
-        /* button  .uib_w_37 */
-    
-    
-        /* button  .uib_w_38 */
-    
-    
-        /* button  .uib_w_39 */
-    $(document).on("click", ".uib_w_39", function(evt)
-    {
-         /*global activate_page */
-         activate_page("#menu"); 
-         return false;
-    });
-    
-        /* button  Info */
-    
-    
-        /* button  1Button */
-    
-    
-        /* button  1Button */
-    
-    
-        /* button  1Button */
-    
-    
-        /* button  1Button */
+        /* button  .uib_w_51 */
     $(document).on("click", ".uib_w_51", function(evt)
     {
-         /* Other options: .modal("show")  .modal("hide")  .modal("toggle")
-         See full API here: http://getbootstrap.com/javascript/#modals 
-            */
-        
-         $(".uib_w_50").modal("toggle");  
+         /*global activate_page */
+         activate_page("#mainpage"); 
          return false;
     });
     
-        /* button  Button */
-    
-    
-        /* button  Button */
-    
-    
-        /* button  Summit2 */
-    
-    
-        /* button  Summit2 */
-    $(document).on("click", ".uib_w_43", function(evt)
-    {
-        /* your code goes here */ 
-         return false;
-    });
-    
-        /* button  Button */
-    
-    
-        /* button  Info2 */
-    
-    
-        /* button  Info3 */
-    
-    
-        /* button  Info4 */
-    
-    
-        /* button  Button */
-    
-    
-        /* button  Info5 */
-    
-    
-        /* button  Info6 */
-    $(document).on("click", ".uib_w_65", function(evt)
-    {
-         /* Other options: .modal("show")  .modal("hide")  .modal("toggle")
-         See full API here: http://getbootstrap.com/javascript/#modals 
-            */
-        
-         $(".uib_w_66").modal("toggle");  
-         return false;
-    });
-    
-        /* button  .uib_w_38 */
-    
-    
-        /* button  .uib_w_38 */
-    $(document).on("click", ".uib_w_38", function(evt)
+        /* button  .uib_w_52 */
+    $(document).on("click", ".uib_w_52", function(evt)
     {
          /*global activate_page */
-         activate_page("#menu"); 
+         activate_page("#dashboard"); 
          return false;
     });
     
-        /* button  .uib_w_37 */
-    $(document).on("click", ".uib_w_37", function(evt)
+        /* button  .uib_w_53 */
+    $(document).on("click", ".uib_w_53", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#dashboard"); 
+         return false;
+    });
+    
+        /* button  Button */
+    $(document).on("click", ".uib_w_54", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#dashboard"); 
+         return false;
+    });
+    
+        /* button  Button */
+    
+    
+        /* button  Button */
+    
+    
+        /* button  .uib_w_57 */
+    
+    
+        /* button  .uib_w_57 */
+    
+    
+        /* button  .uib_w_57 */
+    $(document).on("click", ".uib_w_57", function(evt)
     {
          /*global activate_page */
          activate_page("#pendapatan"); 
          return false;
     });
     
-        /* button  .uib_w_36 */
-    $(document).on("click", ".uib_w_36", function(evt)
+        /* button  .uib_w_56 */
+    $(document).on("click", ".uib_w_56", function(evt)
     {
          /*global activate_page */
          activate_page("#pendapatan"); 
          return false;
     });
     
-        /* button  .uib_w_35 */
-    $(document).on("click", ".uib_w_35", function(evt)
+        /* button  .uib_w_55 */
+    $(document).on("click", ".uib_w_55", function(evt)
     {
          /*global activate_page */
          activate_page("#pendapatan"); 
          return false;
     });
-     
-        /* button  Summit1 */
-    $(document).on("click", ".uib_w_49", function(evt)
+    
+        /* button  Button */
+    $(document).on("click", ".uib_w_58", function(evt)
     {
-        /* your code goes here */ 
+         /*global activate_page */
+         activate_page("#dashboard"); 
          return false;
     });
-     	
-      /* button  Info */
-    $(document).on("click", ".uib_w_60", function(evt)
+    
+        /* button  #send */
+    $(document).on("click", "#send", function(evt)
     {
-   	
-alert("Berfungsi untuk anda melihat up to date ranking anda,Bonus ranking akan di berikan setiap setahun sekali ");
-
+   
         /* your code goes here */ 
          return false;
     });
     
         /* button  Info */
-    $(document).on("click", ".uib_w_62", function(evt)
+    $(document).on("click", ".uib_w_61", function(evt)
     {
         	
 alert("segala bayaran payslip anda akan di update dalam sistem bayaran ini");
@@ -276,45 +243,41 @@ alert("segala bayaran payslip anda akan di update dalam sistem bayaran ini");
     });
     
         /* button  Info */
-    $(document).on("click", ".uib_w_56", function(evt)
+    $(document).on("click", ".uib_w_62", function(evt)
     {
         	
-alert("Segala butiran bank anda perlu di isi untuk memudahkan kami membuat pembayaran ke dalam akaun bank anda");
+alert("Berfungsi untuk anda melihat up to date ranking anda. Bonus ranking akan di berikan setiap setahun sekali");
 
         /* your code goes here */ 
          return false;
     });
     
         /* button  Info */
-    $(document).on("click", ".uib_w_53", function(evt)
+    $(document).on("click", ".uib_w_63", function(evt)
     {
         	
-alert("Jika anda ada idea baru atau ada satu impian untuk memiliki sebuah aplikasi android atau ios. Boleh beri idea anda di sini. kami akan cuba merialisasikan impian anda");
+alert("Segala butiran bank anda perlu di isi untuk memudahkan kami membuat pembayaran ke dalam akaun bank anda. atau pun anda boleh terus whatsapp ke 011-21137801");
 
         /* your code goes here */ 
          return false;
     });
     
         /* button  Info */
-    $(document).on("click", ".uib_w_50", function(evt)
+    $(document).on("click", ".uib_w_64", function(evt)
     {
-        
-alert("anda hanya perlu membiarkan sistem cordova ini bergerak secara aktif");
+        	
+alert("Jika anda ada idea baru atau ada satu impian untuk memiliki sebuah aplikasi android atau ios. Boleh beri idea anda di sini. Kami akan cuba merialisasikan impian anda");
 
         /* your code goes here */ 
          return false;
     });
     
         /* button  Info */
-    $(document).on("click", ".uib_w_50", function(evt)
+    $(document).on("click", ".uib_w_65", function(evt)
     {
-        /* your code goes here */ 
-         return false;
-    });
-    
-        /* button  Info */
-    $(document).on("click", ".uib_w_50", function(evt)
-    {
+        	
+alert("Sila tekan button on sistem dan anda hanya perlu membiarkan sistem cordova bergerak aktif");
+
         /* your code goes here */ 
          return false;
     });
